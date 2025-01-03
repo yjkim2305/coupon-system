9c37007ed56a76f0b21c4510eed25d2be00e2812
KimYoungJun <cyberact3181@gmail.com>
2025-01-03 16:50:48 +0900

    feat: querydsl 설정

---
+++ coupon-core/src/main/java/com/example/couponcore/common/config/QueryDslConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.couponcore.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class QueryDslConfiguration {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
+}
